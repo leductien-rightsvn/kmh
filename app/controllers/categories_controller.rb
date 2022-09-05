b53b7938705683a2bc32d83ee035cde72bbc3d20
Dinh ngoc minh huy <mhuy13660@gmail.com>
2022-09-05 10:00:31 +0700

    add category controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,50 @@
+class CategoriesController < ApplicationController
+
+    before_action :set_category, only: :update
+
+    def index
+        @categories = Category.all
+    end
+
+    def new
+        @category = Category.new
+    end
+
+    def edit
+        @category = Category.find params[:id]
+    end
+
+    def update
+        @category.update category_params
+        redirect_to edit_category_path
+    end
+
+    def destroy
+        @category = Category.find params[:id]
+        @category.destroy
+        redirect_to categories_path
+    end
+
+    def create
+        @category = Category.new category_params
+        if @category.save
+            flash[:success]="Save successfully"
+            redirect_to edit_category_path(@category)
+        else
+            flash[:danger]="Failure"
+            render new
+        end
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit :name, :image
+    end
+
+    private
+
+    def set_category
+        @category = Category.find params[:id]
+    end
+end
