f06a7a1fa6a165c2685ce29f9d65db96c356fec8
Nguyễn Khang <nguyenkhang@rights-s.jp>
2022-09-05 09:27:25 +0700

    create console layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,3 +6,6 @@ import "bootstrap"
 
 import "@hotwired/turbo-rails"
 import "controllers"
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
--- db/schema.rb
+++ db/schema.rb
@@ -21,16 +21,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_19_022959) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  create_table 'active_storage_blobs', charset: 'utf8mb4', collation: 'utf8mb4_bin', force: :cascade do |t|
+    t.string 'key', null: false
+    t.string 'filename', null: false
+    t.string 'content_type'
+    t.text 'metadata'
+    t.string 'service_name', null: false
+    t.bigint 'byte_size', null: false
+    t.string 'checksum'
+    t.datetime 'created_at', null: false
+    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
   end
 
   create_table "active_storage_variant_records", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
