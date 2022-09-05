e80d849e64766bc50668c7645477f79a0ee409c3
Dinh ngoc minh huy <mhuy13660@gmail.com>
2022-09-05 10:56:59 +0700

    add category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,50 +1,47 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: :update
 
-    before_action :set_category, only: :update
+  def index
+    @categories = Category.all
+  end
 
-    def index
-        @categories = Category.all
+  def new
+    @category = Category.new
+  end
+
+  def edit
+    @category = Category.find params[:id]
+  end
+
+  def update
+    @category.update! category_params
+    redirect_to edit_category_path
+  end
+
+  def destroy
+    @category = Category.find params[:id]
+    @category.destroy!
+    redirect_to categories_path
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      flash[:success] = t('save.success')
+      redirect_to edit_category_path(@category)
+    else
+      flash[:danger] = t('save.danger')
+      render new
     end
+  end
 
-    def new
-        @category = Category.new
-    end
-
-    def edit
-        @category = Category.find params[:id]
-    end
-
-    def update
-        @category.update category_params
-        redirect_to edit_category_path
-    end
-
-    def destroy
-        @category = Category.find params[:id]
-        @category.destroy
-        redirect_to categories_path
-    end
-
-    def create
-        @category = Category.new category_params
-        if @category.save
-            flash[:success]="Save successfully"
-            redirect_to edit_category_path(@category)
-        else
-            flash[:danger]="Failure"
-            render new
-        end
-    end
-
-    private
+  private
 
     def category_params
-        params.require(:category).permit :name, :image
+      params.require(:category).permit :name, :image
     end
 
-    private
-
     def set_category
-        @category = Category.find params[:id]
+      @category = Category.find params[:id]
     end
 end
