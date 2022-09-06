3c9ab4fd15b86d2f03e8de18990fb304ae9b2761
Dinh ngoc minh huy <mhuy13660@gmail.com>
2022-09-06 17:10:23 +0700

    add spec

---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe '/', type: :request do
+  describe 'Get /' do
+    it 'renders a successful response' do
+      get root_url
+      expect(response).to be_successful
+    end
+  end
+end
