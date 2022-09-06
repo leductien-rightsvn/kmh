c242d685dbc4382fbdf1699c4e3d5364c1ea7159
Le Duc Tien <leduc.tien@rights-s.jp>
2022-09-06 19:05:47 +0900

    Fix config database

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string(255)
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :user do
     name { FFaker::Name.name }
