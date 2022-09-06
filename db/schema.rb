cc765e954e6aae7cff7d38e14d0eb2d570948cac
Le Duc Tien <leduc.tien@rights-s.jp>
2022-09-06 19:05:47 +0900

    Fix config database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_09_05_042944) do
-  create_table "users", charset: "utf8", collation: "utf8_general_ci", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,7 @@
 #
 require 'rails_helper'
 
-RSpec.describe User do
+RSpec.describe User, type: :model do
   describe 'Validations' do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:name) }
