4e12aba0bfd414395997edf008088d26166e26c7
Dinh ngoc minh huy <mhuy13660@gmail.com>
2022-09-09 15:26:38 +0700

    fix comment 1st

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,15 +3,16 @@
 # Table name: categories
 #
 #  id         :bigint           not null, primary key
-#  name       :string(255)
+#  name       :string(255)      not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 # Indexes
 #
-#  index_categories_on_name  (name) UNIQUE
+#  index_categories_on_name  (name)
 #
 class Category < ApplicationRecord
   has_one_attached :image
-  validates :name, presence: true, length: { maximum: 200, minimum: 6 }, uniqueness: true
+  validates :name, presence: true, length: { maximum: 255 }, uniqueness: true
+  
 end
--- db/migrate/20220908033701_add_name_index_to_category.rb
+++
@@ -1,5 +0,0 @@
-class AddNameIndexToCategory < ActiveRecord::Migration[7.0]
-  def change
-    add_index :categories, :name, unique: true
-  end
-end
