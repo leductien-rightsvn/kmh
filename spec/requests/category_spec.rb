d332b1605e17de1f3b155303e35b9f0d4c0cf5ef
Dinh ngoc minh huy <mhuy13660@gmail.com>
2022-09-09 09:24:18 +0700

    change category_spec

--- spec/requests/category_spec.rb
+++ spec/requests/category_spec.rb
@@ -10,14 +10,8 @@ describe 'Public access to categories', type: :request do
   it "creates a Category and redirects to the Category's edit page" do
     get "/categories/new"
     expect(response).to render_template(:new)
-
     post "/categories", :params => { :category => {:name => "My Category"} }
-
-    # expect(response).to redirect_to(assigns(:category))
-    # follow_redirect!
-
     expect(response).to redirect_to(edit_category_path(assigns(:category)))
-    # expect(response.body).to include("Category was successfully created.")
   end
 
   it "update a Category and redirects to the Category's page" do
