8021ca18df43ceafd12ba410f4363f072747fd31
Dinh ngoc minh huy <mhuy13660@gmail.com>
2022-09-13 14:47:16 +0700

    add file i18n

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,7 +18,7 @@ class CategoriesController < ApplicationController
     @category = Category.new category_params
     respond_to do |format|
       if @category.save
-        format.html { redirect_to categories_url, notice: t('notification.create_success') }
+        format.html { redirect_to categories_url, notice: t('controller.notification.success') }
         format.json { render :index, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -31,7 +31,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to edit_category_url(@category), notice: t('notification.update_success') }
+        format.html { redirect_to edit_category_url(@category), notice: t('controller.notification.success') }
         format.json { render :edit, status: :ok, location: @category }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -43,9 +43,8 @@ class CategoriesController < ApplicationController
   # DELETE /categories/1 or /categories/1.json
   def destroy
     @category.destroy!
-
     respond_to do |format|
-      format.html { redirect_to categories_url, notice: t('notification.delete_success') }
+      format.html { redirect_to categories_url, notice: t('controller.notification.success') }
       format.json { head :no_content }
     end
   end
