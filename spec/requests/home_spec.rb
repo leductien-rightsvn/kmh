6d0912662841530be86e5dd8ba9908bd1ac09a12
nguyenhoangminh-rightsvn <103923179+nguyenhoangminh-rightsvn@users.noreply.github.com>
2022-09-26 10:28:48 +0700

    fix fail cicd

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe 'Home', type: :request do
+RSpec.describe '/', type: :request do
+  before { host! 'lvh.me:3000' }
+
   describe 'Get /' do
     it 'renders a successful response' do
       get root_url
