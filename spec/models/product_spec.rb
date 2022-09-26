3114b9ff3e932c1dfa073ad2f6c13ce1cf8c826a
nguyenhoangminh-rightsvn <103923179+nguyenhoangminh-rightsvn@users.noreply.github.com>
2022-09-26 09:43:19 +0700

    fix conflict ja.yml

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -25,7 +25,7 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'associations' do
-    it { should belong_to(:category) }
+    it { is_expected.to belong_to(:category) }
   end
 
   describe 'Validation' do
