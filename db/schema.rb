b04ed4578c950781223c5fb3c8a03f3024055ba9
nguyenhoangminh-rightsvn <103923179+nguyenhoangminh-rightsvn@users.noreply.github.com>
2022-09-26 09:41:42 +0700

    rebase develop

--- db/schema.rb
+++ db/schema.rb
@@ -57,6 +57,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_09_074611) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "products", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
+    t.string "name", null: false
+    t.float "price", null: false
+    t.integer "quantity", null: false
+    t.string "description"
+    t.string "origin"
+    t.string "color"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -66,4 +79,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_09_074611) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "products", "categories"
 end
